5fe89aba29fe2a4fd526cf1631b06c3cf99c6f55
Fede <75698562+Fede-program@users.noreply.github.com>
2021-08-10 00:40:16 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,72 @@
+window.onload = function() {
+    document.getElementById("red").style.display = "none";
+    document.getElementById("orange").style.display = "none";
+    document.getElementById("yellow").style.display = "none";
+    document.getElementById("green").style.display = "none";
+    document.getElementById("cyan").style.display = "none";
+    document.getElementById("blue").style.display = "none";
+    document.getElementById("purple").style.display = "none";
+};
+
+function colorRed() {
+    var x = document.getElementById("red");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function colorOrange() {
+    var x = document.getElementById("orange");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function colorYellow() {
+    var x = document.getElementById("yellow");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function colorGreen() {
+    var x = document.getElementById("green");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function colorCyan() {
+    var x = document.getElementById("cyan");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function colorBlue() {
+    var x = document.getElementById("blue");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function colorPurple() {
+    var x = document.getElementById("purple");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
